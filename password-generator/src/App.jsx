872b4c0eeaeea4c0124b2e8373818577d6501d7e
Punitsingh-1124 <punitsingh2413@gmail.com>
2025-05-27 21:57:33 +0530

    Password Generator

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,72 +1,76 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import usepasswordgenerator from './hooks/use-password-generator';
 
 const App = () => {
+    const [length, setLength] = useState(4);
+    const [checkboxData, setCheckboxData] = useState([
+        { title: "Include Uppercase Letters", state: false },
+        { title: "Include Lowercase Letters", state: false },
+        { title: "Include Number", state: false },
+        { title: "Include Symbols", state: false },
+    ]);
 
-  const [lenght, setLenght] = useState(4);
-  const [checkboxData, setCheckboxData] = useState([
-    { title: "Include Uppercase Letters", state: false },
-    { title: "Include Lowercase Letters", state: false },
-    { title: "Include Number", state: false },
-    { title: "Include Symbols", state: false },
-  ]);
+    const handleCheckboxChange = (i) => {
+        const updatedCheckboxData = [...checkboxData];
+        updatedCheckboxData[i].state = !updatedCheckboxData[i].state;
+        setCheckboxData(updatedCheckboxData);
+    };
 
+    const { password, errorMessage, generatePassword } = usepasswordgenerator();
 
-  const handleCheckboxChange = (i) => {
-    const updateCheckboxData = [...checkboxData]
-    checkboxData[i].state = !checkboxData[i].state
-  }
-  return (
-    <div className='container'>
-      {/* Password text and copy */}
-      <div className="header">
-        <div className="title">Kh6y#lh35</div>
-        <button
-          className='copyBtn'
-          onClick={() => { }}
-        >
-          Copy
-        </button>
-      </div>
-      {/* Charater lenght */}
-      <div className="charLength">
-        <span>
-          <label>Character Length</label>
-          <label>{lenght}</label>
-        </span>
-        <input
-          type="range"
-          min="4"
-          max="28"
-          value={lenght}
-          onChange={(e) => setLenght(e.target.value)}
-        />
-      </div>
-      {/* checkboxes */}
-      <div className="checkboxes">
-        {checkboxData.map((checkboxe, index) => {
-          return <div key={index}>
-            <input
-              onChange={() => handleCheckboxChange(index)}
-              type="checkbox"
-              checked={checkboxe.state}
-            />
-            <label>{checkboxe.title}</label>
-          </div>
-        })}
-      </div>
-      {/* Strength  */}
+    return (
+        <div className='container'>
+            {/* Password text and copy */}
+            {password && (
+                <div className="header">
+                    <div className="title">{password}</div>
+                    <button className='copyBtn' onClick={() => navigator.clipboard.writeText(password)}>
+                        Copy
+                    </button>
+                </div>
+            )}
 
-      {/* Generate Button  */}
-      <button
-        onClick={() => { }}
-        className='generateBtn'
-      >
-        Generate Password
-      </button>
+            {/* Character length */}
+            <div className="charLength">
+                <span>
+                    <label>Character Length</label>
+                    <label>{length}</label>
+                </span>
+                <input
+                    type="range"
+                    min="4"
+                    max="28"
+                    value={length}
+                    onChange={(e) => setLength(Number(e.target.value))}
+                />
+            </div>
 
+            {/* Checkboxes */}
+            <div className="checkboxes">
+                {checkboxData.map((checkbox, index) => (
+                    <div key={index}>
+                        <input
+                            onChange={() => handleCheckboxChange(index)}
+                            type="checkbox"
+                            checked={checkbox.state}
+                        />
+                        <label>{checkbox.title}</label>
+                    </div>
+                ))}
+            </div>
 
-    </div>
-  )
-}
+            {/* Error Message */}
+            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
 
-export default App
+            {/* Generate Button */}
+            <button
+                onClick={() => generatePassword(checkboxData, length)}
+                className='generateBtn'
+            >
+                Generate Password
+            </button>
+        </div>
+    );
+};
+
+export default App;
