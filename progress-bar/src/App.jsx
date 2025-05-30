79ed5d5d0863121eab02c1208fc5c07809733e9d
Punitsingh-1124 <punitsingh2413@gmail.com>
2025-05-31 00:28:57 +0530

    Progress Bar

--- progress-bar/src/App.jsx
+++ progress-bar/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import ProgressBar from './components/ProgressBar';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const [value, setValue] = useState(0);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setValue((val) => val + 1);
+    }, 100);
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className='App'>
+      <span>Prograss Bar</span>
+      <ProgressBar
+        value={value}
+        onComplete={() => setSuccess(true)}
+      />
+      <span>{success ? "Complete!" : "Loading..."}</span>
+    </div>
+  );
+};
 
 export default App
