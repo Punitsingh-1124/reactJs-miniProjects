f330f8ef2e34a345de5e0268cd20f7e12b77ddfa
Punitsingh-1124 <punitsingh2413@gmail.com>
2025-06-05 22:26:20 +0530

    UseMemo Hook Polyfill

--- useMemo-hook-polyfill/src/App.jsx
+++ useMemo-hook-polyfill/src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const squreValue = () =>{
+    return counter * counter;
+  }
   return (
     <div className='App'>
-        <h2>Counter : 0</h2>
-        <button>Increment</button>
+      <h2>Counter : {counter}</h2>
+      <h2>Squared Counter : {squreValue()}</h2>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
     </div>
   )
 }
